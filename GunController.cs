6ff104447fdaef185db9850ceadc1cc86c49e2db
Bhavam Vidyathi <bhavam.vidyarthi@gmail.com>
2019-12-22 00:01:27 +0530

    Initial Commit

---
+++ GunController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+   public Transform weaponHold;
+   Gun equippedGun;  
+   public Gun startingGun;
+   void Start()
+   {
+       if(startingGun != null)
+       {
+           EquipGun(startingGun);
+       }
+   }
+   public void EquipGun(Gun gunToEquip)
+   {
+      if(equippedGun != null)
+      {
+          Destroy(equippedGun.gameObject);
+      }
+      equippedGun=Instantiate(gunToEquip) as Gun; //changed as some pos error
+      equippedGun.transform.parent=weaponHold; //see how this works
+   }
+   public void Shoot()
+   {
+       if(equippedGun != null)
+       {
+           equippedGun.Shoot();
+       }
+   }
+}
---
+++ Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float speed=10;  
+    public LayerMask collisionMask;
+     // Update is called once per frame
+    public void SetSpeed(float newSpeed)
+    {
+        speed=newSpeed;
+    }
+    void Update()
+    {
+      float moveDistance=speed*Time.deltaTime;
+      CheckCollisions(moveDistance);
+      transform.Translate(Vector3.forward*moveDistance);  
+      
+    }
+    void CheckCollisions(float moveDistance)
+    {
+      Ray ray=new Ray (transform.position,transform.forward);
+      RaycastHit hit;
+      if(Physics.Raycast(ray,out hit,moveDistance,collisionMask,QueryTriggerInteraction.Collide))
+      {
+          OnHitObject(hit);
+      } 
+    }
+    void OnHitObject(RaycastHit hit)
+    {
+        Debug.Log(hit.collider.gameObject.name);
+        Destroy(gameObject); //changes from SebLague used Destroy instead of Gameobject.Destroy
+    }
+}
